856558f2194685e707a14813bc95fd129629e6a1
Punnapat2546 <pannapatanai.pun@mail.kmutt.ac.th>
2023-03-14 23:35:35 +0700

    final

--- Lab3_ICOC/Core/Src/main.c
+++ Lab3_ICOC/Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#define IC_BUFFER_SIZE 20
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -40,20 +40,32 @@
 
 /* Private variables ---------------------------------------------------------*/
 TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim2;
+DMA_HandleTypeDef hdma_tim2_ch1;
 
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-uint32_t MotorSetDuty = 500;
+uint32_t MotorSetDuty = 0;
+float averageRisingEdgePeriod;
+uint32_t InputCaptureBuffer[IC_BUFFER_SIZE];
+float MotorReadRPM;
+uint8_t MotorControlEnable = 0;
+uint32_t MotorSetRPM = 0;
+uint32_t DutyToRPM = 0;
+float duty;
+uint8_t check_control_en = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_TIM1_Init(void);
+static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
-
+float IC_Calc_Period();
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -89,11 +101,16 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_USART2_UART_Init();
   MX_TIM1_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
 HAL_TIM_Base_Start(&htim1);
 HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+
+HAL_TIM_Base_Start(&htim2);
+HAL_TIM_IC_Start_DMA(&htim2, TIM_CHANNEL_1, InputCaptureBuffer, IC_BUFFER_SIZE);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -104,10 +121,49 @@ HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
 
     /* USER CODE BEGIN 3 */
 	  static uint32_t timestamp = 0;
-	  if (HAL_GetTick() >= timestamp) {
-		timestamp = HAL_GetTick()+500;
-		__HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_1,MotorSetDuty);
-	}
+	  if (HAL_GetTick() - timestamp >= 500)
+	  {
+		  timestamp = HAL_GetTick();
+		  averageRisingEdgePeriod = IC_Calc_Period();
+		  MotorReadRPM = (5000000/averageRisingEdgePeriod)/64;
+
+		  switch (MotorControlEnable) {
+		  	case 0:
+		  		check_control_en = 0;
+				__HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_1,MotorSetDuty*10);
+				break;
+			case 1:
+				if (check_control_en == 0) {
+					duty = MotorSetDuty;
+					check_control_en = 1;
+				}
+				if (MotorReadRPM < MotorSetRPM && duty < 100) {
+					if (MotorReadRPM - MotorSetRPM >= -1 && MotorReadRPM - MotorSetRPM <= -0.1) {
+						duty += 0.1;
+					}
+					else if(MotorReadRPM - MotorSetRPM < -1){
+						duty += 1;
+					}
+				}
+				else if (MotorReadRPM > MotorSetRPM && duty > 0) {
+					if (MotorReadRPM - MotorSetRPM <= 1 && MotorReadRPM - MotorSetRPM >= 0.1) {
+						duty -= 0.1;
+					}
+					else if (MotorReadRPM - MotorSetRPM >= 1) {
+						duty -= 1;
+					}
+				}
+
+
+
+				__HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_1,duty*10);
+				break;
+		}
+
+
+	  }
+
+
   }
   /* USER CODE END 3 */
 }
@@ -233,6 +289,64 @@ static void MX_TIM1_Init(void)
 }
 
 /**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_IC_InitTypeDef sConfigIC = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 83;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 4294967295;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_IC_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
+  sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+  sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+  sConfigIC.ICFilter = 0;
+  if (HAL_TIM_IC_ConfigChannel(&htim2, &sConfigIC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+
+}
+
+/**
   * @brief USART2 Initialization Function
   * @param None
   * @retval None
@@ -262,6 +376,22 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Stream5_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Stream5_IRQn);
 
 }
 
@@ -299,7 +429,26 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+float IC_Calc_Period()
+{
+    uint32_t currentDMAPointer = IC_BUFFER_SIZE - __HAL_DMA_GET_COUNTER((htim2.hdma[1]));
 
+    uint32_t lastValidDMAPointer = (currentDMAPointer-1 + IC_BUFFER_SIZE)%IC_BUFFER_SIZE;
+
+    uint32_t i = (lastValidDMAPointer - 5 + IC_BUFFER_SIZE)%IC_BUFFER_SIZE;
+
+    int32_t sumdiff = 0;
+    while(i!=lastValidDMAPointer)
+    {
+        uint32_t firstCapture  = InputCaptureBuffer[i];
+
+        uint32_t NextCapture = InputCaptureBuffer[(i+1)%IC_BUFFER_SIZE];
+
+        sumdiff += NextCapture - firstCapture;
+        i = (i+1)% IC_BUFFER_SIZE;
+    }
+    return sumdiff / 5.0;
+}
 /* USER CODE END 4 */
 
 /**
